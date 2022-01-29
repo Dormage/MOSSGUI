44b93672958cf5a9ae0ff66833b5c53d6664183c
Aleksandar <aleksandar.tosic@upr.si>
2022-01-29 12:45:29 +0100

    init

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+    id("org.openjfx.javafxplugin")  version "0.0.10"
+    application
+}
+
+group = "me.dormage"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+
+dependencies{
+    implementation("org.openjfx:javafx:17.0.2")
+    javafx {
+        modules("javafx.controls", "javafx.fxml", "javafx.media")
+    }
+    implementation("io.github.bitstorm:tinyzip-core:1.0.0")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+application {
+    mainClass.set("Main")
+}
