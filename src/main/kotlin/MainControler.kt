ef54f2a0d36ed5bd57e5aff67cc5c10c04b0bd44
Aleksandar <aleksandar.tosic@upr.si>
2022-02-03 23:12:07 +0100

    fix loading bar, and progress log. Begin working on preview table before submitting to MOSS

--- src/main/kotlin/MainControler.kt
+++ src/main/kotlin/MainControler.kt
@@ -1,11 +1,10 @@
 import javafx.application.Platform
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
-import javafx.fxml.FXMLLoader
-import javafx.scene.Scene
+import javafx.scene.Parent
 import javafx.scene.control.ProgressBar
+import javafx.scene.control.TextField
 import javafx.scene.layout.AnchorPane
-import javafx.scene.layout.BorderPane
 import javafx.stage.DirectoryChooser
 import javafx.stage.Stage
 import org.apache.commons.io.FileUtils
@@ -33,52 +32,62 @@ class MainControler(private val stage: Stage, private val main: Main){
     @FXML
     private lateinit var mainPane : AnchorPane
     @FXML
+    private lateinit var loadingStatusLog : TextField
+
+    @FXML
     private fun loadAssignments(event: ActionEvent) {
         DirectoryChooser().apply {
             title = "Select folder containing assignments"
             showDialog(stage)?.apply {
-                val fxmlLoader = FXMLLoader(MainControler::class.java.getResource("LoadingFiles.fxml"))
-                fxmlLoader.setController(this)
-                val yourNewView = fxmlLoader.load<BorderPane>()
-                mainPane.children.setAll(yourNewView)
+                val new :Parent = main.loadComponent("LoadingFiles.fxml", this@MainControler)
+                mainPane.children.setAll(new)
                 //creates temporary directory following required MOSS submission structure
                 val tempDirectory: Path = Files.createTempDirectory("MOSS_")
                 println("Created temporary folder at:  $tempDirectory")
                 dataManager.url = absoluteFile.toString()
                 val thread = Thread() {
-                    Thread.sleep(1000)
                     var currentProgress = 0
                     val submissionDirs: Array<File> = absoluteFile.listFiles(DirectoryFileFilter.DIRECTORY as FileFilter)
-                    submissionDirs.forEach {
-                        println("Processing student : ${it.name}$ ")
+                    submissionDirs.forEachIndexed { index, assignmentFolder ->
+                        println("Processing student : ${assignmentFolder.name}$ ")
+                        logLoadingProgress("Processing student : ${assignmentFolder.name}$ ...")
                         var error: String = ""
-                        val compressed: Sequence<File> = it.walkTopDown().filter { it.toString().endsWith(".zip") }
-                        compressed.forEachIndexed { index, file ->
+                        val compressed: Sequence<File> = assignmentFolder.walkTopDown().filter { assignmentFolder.toString().endsWith(".zip") }
+                        compressed.forEach {file ->
                             try {
-                                println("Uncompressing ...")
+                                logLoadingProgress("Uncompressing assignment ...")
                                 TinyZip.unzip(file.path.toString(), file.parent.toString())
                             } catch (e: Exception) {
                                 error = e.toString()
                             }
                         }
                         //create temp submission folder
-                        val tempStudentDir = Files.createDirectory(Paths.get(tempDirectory.toString() + File.separator + it.name+File.separator))
-                        FileUtils.listFiles(File(it.toString()), arrayOf("java"), true).forEach{ sourceFile ->
-                            val destination: File = File(tempStudentDir.toString() + File.separator + sourceFile.name)
+                        val tempStudentDir = Files.createDirectory(Paths.get(tempDirectory.toString() + File.separator + assignmentFolder.name+File.separator))
+                        val sourceCodeFiles = FileUtils.listFiles(File(assignmentFolder.toString()), arrayOf("java"), true)
+                        sourceCodeFiles.forEach{ sourceFile ->
+                            val destination = File(tempStudentDir.toString() + File.separator + sourceFile.name)
                             Files.copy(sourceFile.toPath(),destination.toPath(),StandardCopyOption.REPLACE_EXISTING)
-                            println("Copy command from ${sourceFile.toPath().toString()} to : $destination")
+                            println("Copy command from ${sourceFile.toPath()} to : $destination")
+                            logLoadingProgress("Copying source files to $destination")
                         }
+                        dataManager.addStudent(Student(index, assignmentFolder.name, tempStudentDir.toString(), error, sourceCodeFiles.toMutableList()))
 
                         Platform.runLater(Runnable {
                            loadProgress.progress = currentProgress.toDouble() / submissionDirs.size
                         })
                         currentProgress++
                     }
+                    val preview :Parent = main.loadComponent("PreviewAssignments.fxml", SettingsControler(stage,main))
+                    stage.scene.root = preview
                 }.start()
             }
         }
     }
-
+    private fun logLoadingProgress(progress:String){
+        Platform.runLater(Runnable {
+            loadingStatusLog.text = progress
+        })
+    }
     @FXML
     private fun loadHistory(){
 
